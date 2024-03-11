81744172c226fa4f061f0f69814d4c8ecbe7eb92
Nikolas Rummel <nikolasrummel6@gmail.com>
2024-03-11 08:56:06 +0100

    added exercices

--- src/main/java/de/dhbw/nikolas/exercise/io/TextFile.java
+++ src/main/java/de/dhbw/nikolas/exercise/io/TextFile.java
@@ -9,7 +9,14 @@ public class TextFile {
 
     private List<String> lines;
 
+    private String path;
+
     public TextFile(String path) {
+        this.path = path;
+        this.read();
+    }
+
+    public void read() {
         try {
             this.lines = Files.readAllLines(Path.of(path));
         } catch (IOException e) {
@@ -17,8 +24,42 @@ public class TextFile {
         }
     }
 
-    public void read() {
-
+    public void write() {
+        try {
+            Files.write(Path.of(path), lines);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    public int getAvailableLines() {
+        return lines.size();
+    }
+
+    public String[] getLines() {
+        String[] lines = new String[this.lines.size()];
+
+        for (int i = 0; i < this.lines.size(); i++) {
+            lines[i] = this.lines.get(i);
+        }
+        return lines;
+    }
+
+    public String getLine(int index) {
+        return lines.get(index);
+    }
+
+    public void setLine(int index, String line) {
+        this.lines.set(index, line);
+    }
+
+    public void replaceAll(String regexp, String ersatz) {
+        for (int i = 0; i < lines.size(); i++) {
+            lines.set(i, lines.get(i).replaceAll(regexp, ersatz));
+        }
+    }
+
+    public void close(){
+        this.write();
+    }
 }
